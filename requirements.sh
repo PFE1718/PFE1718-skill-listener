cdd13ebe3529830c7dfd21342f436193a32e441e
RReivax <xavier.hermand@edu.ece.fr>
2018-02-04 05:59:07 -0800

    Fixed requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,7 @@
 #This file is for any packages you need installed for your skill to run
 if [ ! -d "/opt/mycroft/skills/PFE1718-skill-listener/habits" ]; then
    mkdir /opt/mycroft/skills/PFE1718-skill-listener/habits
-   touch /opt/mycroft/skills/PFE1718-skill-listener/logs.json
+   touch /opt/mycroft/skills/PFE1718-skill-listener/habits/logs.json
    echo '[]' > /opt/mycroft/skills/PFE1718-skill-listener/habits/habits.json
    echo '[]' > /opt/mycroft/skills/PFE1718-skill-listener/habits/triggers.json
 #   sudo chmod -R ugo+rw /opt/mycroft/habits
