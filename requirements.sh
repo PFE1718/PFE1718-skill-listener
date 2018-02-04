c8f616bfc814c819964ceec3b610adcac577e473
RReivax <xavier.hermand@edu.ece.fr>
2018-02-04 05:56:51 -0800

    Changed habits folder to reposository

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,6 @@
 #This file is for any packages you need installed for your skill to run
 if [ ! -d "/opt/mycroft/skills/PFE1718-skill-listener/habits" ]; then
-   sudo mkdir /opt/mycroft/habits
+   mkdir /opt/mycroft/skills/PFE1718-skill-listener/habits
    touch /opt/mycroft/skills/PFE1718-skill-listener/logs.json
    echo '[]' > /opt/mycroft/skills/PFE1718-skill-listener/habits/habits.json
    echo '[]' > /opt/mycroft/skills/PFE1718-skill-listener/habits/triggers.json
